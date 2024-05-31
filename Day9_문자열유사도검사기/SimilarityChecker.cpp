913a7137090dd765ff47f91b288bd77ebb00b194
min.byun <leaftotoro@gmail.com>
2024-05-31 10:44:34 +0900

    git add alphabet test

--- Day9_문자열유사도검사기/SimilarityChecker.cpp
+++ Day9_문자열유사도검사기/SimilarityChecker.cpp
@@ -1,4 +1,7 @@
 #include <string>
+#include <vector>
+
+#define ALPHA_CNT 26
 
 using namespace std;
 
@@ -7,13 +10,40 @@ public:
 	int checkLength(string str_A, string str_B) {
 		calcLength(str_A, str_B);
 		calcMinMaxLength();
-		if (checkSameLength()) return MAX_SCORE;
+		if (checkSameLength()) return MAX_SCORE_LENGTH;
 		if (check2xDiffLengh())return MIN_SCORE;
 		return calculateSocre();
 	}
+
+	int checkAlphabet(string str_A, string str_B) {
+		int score = 0;
+		for (auto ch : str_A) {
+			int idx = ch - 'A';
+			cntAlphabetStr[idx] = cntAlphabetStrA[idx] = true;
+		}
+		for (auto ch : str_B) {
+			int idx = ch - 'A';
+			cntAlphabetStr[idx] = cntAlphabetStrB[idx] = true;
+		}
+
+
+		int sameCnt = 0;
+		int totalCnt = 0;
+		for (int i = 0; i < ALPHA_CNT; i++) {
+			if (cntAlphabetStr[i] == false) continue;
+			totalCnt++;
+			if (cntAlphabetStrA[i] == cntAlphabetStrB[i])
+				sameCnt++;
+		}
+
+		score = (double)(sameCnt * MAX_SCORE_ALPHABET) / totalCnt;
+		return score;
+	}
 private:
-	const int MAX_SCORE = 60;
+	const int MAX_SCORE_LENGTH = 60;
+	const int MAX_SCORE_ALPHABET = 40;
 	const int MIN_SCORE = 0;
+
 	int len_A = 0;
 	int len_B = 0;
 	int len_min = 0;
@@ -38,6 +68,10 @@ private:
 	}
 
 	int calculateSocre() {
-		return (1 - (double)(len_max - len_min) / len_min) * MAX_SCORE;
+		return (1 - (double)(len_max - len_min) / len_min) * MAX_SCORE_LENGTH;
 	}
+
+	bool cntAlphabetStrA[ALPHA_CNT] = { 0, };
+	bool cntAlphabetStrB[ALPHA_CNT] = { 0, };
+	bool cntAlphabetStr[ALPHA_CNT] = { 0, };
 };
